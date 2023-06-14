92ded35102a066c6cfe0627bcbfae91eabb3c97f
Franco <francosaldano1@gmail.com>
2023-06-14 00:34:32 -0300

    responsive details

--- src/components/Main/Aside.jsx
+++ src/components/Main/Aside.jsx
@@ -1,13 +1,11 @@
-const Aside = () => {
+const Aside = ({ products }) => {
   return (
     <div className="row-span-5 col-span-1 fixed">
-      <div className="w-max text-amber-400 absolute top-60 -left-14 sm:-left-42 text-right font-title-hammer font-bold scale-120 -rotate-90">
-        <h3 className="my-1 text-6xl xl:text-8xl">CODER</h3>
-        <h3 className="my-1 text-6xl xl:text-8xl">COMPANY </h3>
-        <h3 className="my-1 text-md xl:text-2xl">DESARROLLO WEB</h3>
+      <div className="w-max text-amber-400 absolute top-44 sm:-left-6 text-right font-title-hammer font-bold scale-120 -rotate-90">
+        <h3 className="my-1 text-3xl sm:text-6xl xl:text-8xl">CODER</h3>
+        <h3 className="my-1 text-3xl sm:text-6xl xl:text-8xl">COMPANY </h3>
       </div>
     </div>
   )
 }
 export default Aside
-//mx-0 mt-32 mb-18 md:-mx-0 md:mt-32
